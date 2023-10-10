2ea8323240a0914798939a4a045a59e56a10e6ae
stnDinus <111202214433@mhs.dinus.ac.id>
2023-10-10 09:30:43 +0700

    docs: HW3 dda documentation

--- HW3/lib/dda.hxx
+++ HW3/lib/dda.hxx
@@ -3,4 +3,10 @@
 #include <cstdlib>
 #include <vector>
 
-std::vector<double> dda(std::vector<double> titik_awal, std::vector<double> titik_akhir);
+/**
+ * @param titik_awal vector berisi 2 nilai posisi (x, y)
+ * @param titik_akhir vector berisi 2 nilai posisi (x, y)
+ * @returns vector berisi nilai-nilai posisi setiap titik
+ */
+std::vector<double> dda(std::vector<double> titik_awal,
+                        std::vector<double> titik_akhir);
