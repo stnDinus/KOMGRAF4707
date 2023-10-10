59a1a345cfac33fb94555ecedfb4f77971c45a89
stnDinus <111202214433@mhs.dinus.ac.id>
2023-10-10 10:01:18 +0700

    feat: HW3 input start & end points

--- HW3/main.cxx
+++ HW3/main.cxx
@@ -2,10 +2,17 @@
 
 #include <iostream>
 
-int main() {
-  std::vector<double> titik_awal = {-9, 4};
-  std::vector<double> titik_akhir = {9, -1};
-  std::vector<double> dda_arr = dda(titik_awal, titik_akhir);
-  for (int i = 0; i < dda_arr.size(); i++)
-    std::cout << dda_arr[i] << ',' << dda_arr[++i] << std::endl;
+using namespace std;
+
+int main(int argc, char **argv) {
+  if (argc != 5) {
+    cerr << "masukan 4 argumen: 1x 1y 2x 2y" << endl;
+    return -1;
+  }
+
+  vector<double> titik_awal = {stod(argv[1]), stod(argv[2])};
+  vector<double> titik_akhir = {stod(argv[3]), stod(argv[4])};
+  vector<double> dda_vec = dda(titik_awal, titik_akhir);
+  for (int i = 0; i < dda_vec.size(); i++)
+    cout << dda_vec[i] << ',' << dda_vec[++i] << endl;
 }
