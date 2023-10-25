fc6d39ca3dddfb8c6a7e9ef840042d5a7bc66080
stnDinus <111202214433@mhs.dinus.ac.id>
2023-10-25 13:18:54 +0700

    fix: HW5 Matriks2d->Matriks, A B, print

--- HW5/main.cxx
+++ HW5/main.cxx
@@ -1,11 +1,32 @@
 #include "Matriks.hxx"
+#include <iostream>
 
+using namespace std;
 int main(int argc, char *argv[]) {
-  Matriks2d m = Matriks2d(2);
-  m.init_acak();
-  m.print();
-  const int n = 2;
-  int arr_penambah[n] = {1, 1};
-  m.translasi_matriks(arr_penambah, n);
-  m.print();
+  // A11.2022.14433
+  Matriks A = Matriks(2, 1);
+  A.set_elemen(0, 0, 3);
+  A.set_elemen(1, 0, 3);
+
+  Matriks B = Matriks(2, 1);
+  B.set_elemen(0, 0, 4);
+  B.set_elemen(1, 0, 4);
+
+  cout << "Matriks Awal\n";
+  cout << "Matriks A\n";
+  A.print();
+  cout << "Matriks B\n";
+  B.print();
+  cout << endl;
+
+  int arr_penambah[] = {5, 10};
+  A.translasi_matriks(arr_penambah, 2);
+  B.translasi_matriks(arr_penambah, 2);
+
+  cout << "Translasi(5, 10)\n";
+  cout << "Matriks A\n";
+  A.print();
+  cout << "Matriks B\n";
+  B.print();
+  cout << endl;
 }
